b890227a08cdaca7c8ad89729ed4b6b889c8b368
David23112006 <159440870+David23112006@users.noreply.github.com>
2024-08-23 11:15:15 -0300

    Update desafio12.js

--- desafio12.js
+++ desafio12.js
@@ -1,13 +1,19 @@
 var jogahtml = window.document.getElementById('saida')
 
 function clique1() {
-    jogahtml.innerHTML += '<p>Você clicou escolheu a primeira opção = botão 1</p>'
+jogahtml.innerHTML += '<p> Você clicou escolheu a primeira opção = botão 1 </p>'
+
 }
 
 function clique2() {
-    jogahtml.innerHTML += '<p>Você clicou escolheu a segunda opção = botão 2</p>'
+jogahtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 2 </p>'
+
 }
 
 function clique3() {
-    jogahtml.innerHTML += '<p>Você clicou escolheu a terceira opção = botão 3</p>'
+jogahtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 3</p>'
 }
+
+function clique4() {
+    jogahtml.innerHTML += '<p> Você Clicou escolheu a primeira opção = botão 4</p>'
+    }
